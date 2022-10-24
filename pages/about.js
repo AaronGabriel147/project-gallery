7ad92cb00714e43deb4cb8da06fc8b85484d9faf
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-10-24 15:59:03 -0700

    fixed error

--- pages/about.js
+++ pages/about.js
@@ -12,7 +12,7 @@ import newGithub from '../public/newGithub.jpg';
 export default function About() {
     return (
         <div className="app">
-            <NavBar />
+            {/* <NavBar /> */}
             <h1 id="about-h1">About</h1>
 
             <div className="about-card">
@@ -26,7 +26,7 @@ export default function About() {
                 // priority
                 />
                 <p>Hello, I am Aaron Gabriel, a full stack web developer with a strong dedication for creating beautiful, user-friendly websites. I have a background in web development and have a passion for creativity and problem solving. I am positive and resilient, I work well with others and love creating real world projects.</p>
-                <p>Some of my interests: I enjoy building and programming mechanical keyboards. I love science (astronomy, paleoanthropology, & evolution) as well as philosophy (ancient Greek & Roman Stoicism). My motivation for success is my family, I am married and have two daughters (4 & 6). There's nothing I enjoy more than being a parent. My passion and resilience for being a successful developer stems from my girls.</p>
+                <p>Some of my interests: I enjoy building and programming mechanical keyboards. I love science (astronomy, paleoanthropology, & evolution) as well as philosophy (ancient Greek & Roman Stoicism). My motivation for success is my family, I am married and have two daughters (4 & 6). There is nothing I enjoy more than being a parent. My passion and resilience for being a successful developer stems from my girls.</p>
             </div>
 
             {/* LinkedIn */}
