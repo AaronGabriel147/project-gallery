84130d8c9ee7a1674bfefd9f6ea153301706567e
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-10-24 15:46:26 -0700

    fixed error

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -1,22 +1,22 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 
 
-function NavBar() {
-    return (
-        <div className="nav">
-            <ul className="nav">
+// function NavBar() {
+//     return (
+//         <div className="nav">
+//             <ul className="nav">
 
-                <Link href="/">
-                    <li>Projects</li>
-                </Link>
+//                 <Link href="/">
+//                     <li>Projects</li>
+//                 </Link>
 
-                <Link href="/about">
-                    <li>About</li>
-                </Link>
+//                 <Link href="/about">
+//                     <li>About</li>
+//                 </Link>
 
-            </ul>
-        </div >
-    )
-}
+//             </ul>
+//         </div >
+//     )
+// }
 
-export default NavBar
+// export default NavBar
