c998d6b9db22aacadec49185ecc39b6d07b90ee2
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-10-24 15:52:53 -0700

    fixed error

--- pages/Footer.js
+++ pages/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
         <div className="about-footer">
             <div className="line"></div>
             <p id="footer-txt">Created by Aaron Gabriel</p>
-            <a id="footer-txt" href="https://github.com/AaronGabriel147/project-gallery">This web app was built with Next.js and has all of the 'Next/Future/Image' optimizations for extremely fast load times.</a>
+            <a id="footer-txt" href="https://github.com/AaronGabriel147/project-gallery">This web app was built with Next.js and has all of the Next/Future/Image optimizations for extremely fast load times.</a>
         </div >
     )
 }
