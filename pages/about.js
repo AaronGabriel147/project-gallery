49e6598d6a926971952606b6601c25d2e8ea8bc8
Aaron Gabriel <aarongabriel147@gmail.com>
2024-10-09 09:54:34 -0700

    Had to use backticks to escape a single quote mark

--- pages/about.js
+++ pages/about.js
@@ -25,7 +25,7 @@ export default function About() {
                     quality="100"
                 // priority
                 />
-                <p>Hello, I'm Aaron Gabriel, a passionate and dedicated Full Stack Web Developer specializing in creating engaging, responsive, and user-friendly websites. I bring a strong technical background in frontend and backend development, focusing on delivering seamless user experiences. With expertise in modern JavaScript frameworks, including React.js, and experience in Shopify development, I am committed to building solutions that solve real-world problems and delight users.</p>
+                <p>{`Hello, I'm Aaron Gabriel, a passionate and dedicated Full Stack Web Developer specializing in creating engaging, responsive, and user-friendly websites. I bring a strong technical background in frontend and backend development, focusing on delivering seamless user experiences. With expertise in modern JavaScript frameworks, including React.js, and experience in Shopify development, I am committed to building solutions that solve real-world problems and delight users.`}</p>
                 <p>I am a proactive team player who thrives in collaborative environments, and I take pride in my ability to adapt and learn quickly. My creative mindset and problem-solving skills enable me to tackle challenging projects with a positive attitude.</p>
             
                 <a id="link" style={{ padding   : '24px' }} href="mailto:aarongabriel147@gmail.com">Contact Me</a>
