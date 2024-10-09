86008605bd4d9472ca5162b95291b3579cc1e80f
Aaron Gabriel <aarongabriel147@gmail.com>
2024-10-09 11:07:15 -0700

    about card bg and footer spacing

--- pages/Footer.js
+++ pages/Footer.js
@@ -9,16 +9,17 @@ export default function Footer() {
             <div class="footer-container">
                 <ul class="link">
 
-                    <Link href="/">
-                        <li>Projects</li>
+                    <Link href="/about">
+                        <li class="footer-link">About</li>
                     </Link>
 
-                    <Link href="/about">
-                        <li>About</li>
+
+                    <Link href="/">
+                        <li class="footer-link">Projects</li>
                     </Link>
 
                     {/* style={{ padding   : '24px' }}  */}
-                    <a class="link" href="mailto:aarongabriel147@gmail.com">Contact Me</a>
+                    <a class="link" id="contact-me" href="mailto:aarongabriel147@gmail.com">Contact Me</a>
 
                 </ul>
 
