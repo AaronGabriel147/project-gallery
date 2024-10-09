563eab1e83265b51b5d03eaaa7952a6f4d3c045e
Aaron Gabriel <aarongabriel147@gmail.com>
2024-10-09 13:47:12 -0700

    added logo

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -1,9 +1,25 @@
 import Link from 'next/link'
+import Image from 'next/future/image'
+import phoenixLogo from '../public/phoenixLogo.png';
+
 
 
 function NavBar() {
     return (
         <div className="nav">
+
+<div class="nav-logo">
+    <Image
+        src={phoenixLogo}
+        alt='project'
+        placeholder='blur'
+        layout="responsive"
+        quality="100"
+        priority
+        width={70}
+    />
+</div>
+
             <ul className="nav">
 
                 <Link href="/">
